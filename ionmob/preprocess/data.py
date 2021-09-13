94802e4ae8e9ebdd870b474f69c05a2076160663
theGreatHerrLebert <davidteschner@googlemail.com>
2021-09-13 16:53:43 +0200

    sequence end dropping as parameter

--- ionmob/preprocess/data.py
+++ ionmob/preprocess/data.py
@@ -3,14 +3,14 @@ import tensorflow as tf
 from ionmob.preprocess.helpers import sequence_to_tokens, get_helix_score, get_gravy_score
 
 
-def get_tf_dataset(mz, charge, sequence, ccs, tokenizer):
+def get_tf_dataset(mz, charge, sequence, ccs, tokenizer, drop_sequence_ends: bool = False):
     """
     takes data and puts them into a tensorflow dataset for easy tf interop
     :param mz: arrays of mz values
     :param charge: arrays of one hot encoded charge state values between 1 and 4 (one hot 0 to 3)
     :param sequence: array of sequences as strings
     :param ccs: array of ccs values
-    :param tokenizer_path: path to a pre-fit tokenizer
+    :param tokenizer: pre-fit tokenizer
     :return: a tensorflow dataset for prediction
     """
     if ccs is not None:
@@ -28,13 +28,13 @@ def get_tf_dataset(mz, charge, sequence, ccs, tokenizer):
                                                     helix_score, gravy_score), dummy_ccs))
 
 
-def get_prediction_data(mz, charge, sequence, tokenizer):
+def get_prediction_data(mz, charge, sequence, tokenizer, drop_sequence_ends):
     """
     takes data for prediction and preprocesses it
     :param mz: arrays of mz values
     :param charge: arrays of one hot encoded charge state values between 1 and 4 (one hot 0 to 3)
     :param sequence: array of sequences as strings
-    :param tokenizer_path: path to a prefittet tokenizer
+    :param tokenizer: pre-fit tokenizer
     :return: a tensorflow dataset for prediction
     """
     # prepare masses
@@ -42,7 +42,7 @@ def get_prediction_data(mz, charge, sequence, tokenizer):
     # prepare charges
     charges_one_hot = tf.one_hot(charge - 1, 4)
 
-    seq_tokens = [sequence_to_tokens(s) for s in sequence]
+    seq_tokens = [sequence_to_tokens(s, drop_sequence_ends) for s in sequence]
     seq_padded = tf.keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences(seq_tokens), 40,
                                                                padding='post')
 
@@ -54,7 +54,7 @@ def get_prediction_data(mz, charge, sequence, tokenizer):
     return masses, charges_one_hot, seq_padded, helix_score, gravy_score
 
 
-def get_training_data(mz, charge, sequence, ccs, tokenizer_path):
+def get_training_data(mz, charge, sequence, ccs, tokenizer, drop_sequence_ends):
     """
     takes data for training and preprocesses it
     :param mz: arrays of mz values
@@ -68,7 +68,7 @@ def get_training_data(mz, charge, sequence, ccs, tokenizer_path):
     ccs = np.expand_dims(ccs, 1)
 
     masses, charges_one_hot, seq_padded, helix_score, gravy_score = get_prediction_data(mz, charge, sequence,
-                                                                                        tokenizer_path)
+                                                                                        tokenizer, drop_sequence_ends)
 
     # generate dataset
     return masses, charges_one_hot, seq_padded, helix_score, gravy_score, ccs
