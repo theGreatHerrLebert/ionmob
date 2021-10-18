586871080b32bbd00c3cde2559b8bb413f40711d
theGreatHerrLebert <davidteschner@googlemail.com>
2021-10-18 10:08:28 +0200

    redone attention model

--- ionmob/models/deep_models.py
+++ ionmob/models/deep_models.py
@@ -2,113 +2,6 @@ import numpy as np
 import tensorflow as tf
 
 
-class SelfAttentionLayer(tf.keras.layers.Layer):
-    def __init__(self, d):
-        super(SelfAttentionLayer, self).__init__()
-        self.d = d
-
-    def build(self, input_shape):
-        self.Wq = self.add_weight(
-            shape=(input_shape[-1], self.d), initializer='glorot_uniform',
-            trainable=True, dtype='float32')
-
-        self.Wk = self.add_weight(
-            shape=(input_shape[-1], self.d), initializer='glorot_uniform',
-            trainable=True, dtype='float32')
-
-        self.Wv = self.add_weight(
-            shape=(input_shape[-1], self.d), initializer='glorot_uniform',
-            trainable=True, dtype='float32')
-
-    def call(self, q_x, k_x, v_x, mask=None):
-        q = tf.matmul(q_x, self.Wq)
-        k = tf.matmul(k_x, self.Wk)
-        v = tf.matmul(v_x, self.Wv)
-        p = tf.matmul(q, k, transpose_b=True) / tf.math.sqrt(np.float32(self.d))
-
-        if mask is None:
-            p = tf.nn.softmax(p)
-        else:
-            p += mask * -1e9
-            p = tf.nn.softmax(p)
-
-        h = tf.matmul(p, v)
-        return h, p
-
-
-class FCLayer(tf.keras.layers.Layer):
-    def __init__(self, d1, d2):
-        super(FCLayer, self).__init__()
-        self.d1 = d1
-        self.d2 = d2
-
-    def build(self, input_shape):
-        self.W1 = self.add_weight(
-            shape=(input_shape[-1], self.d1), initializer='glorot_uniform',
-            trainable=True, dtype='float32'
-        )
-        self.b1 = self.add_weight(
-            shape=(self.d1,), initializer='glorot_uniform',
-            trainable=True, dtype='float32'
-        )
-        self.W2 = self.add_weight(
-            shape=(input_shape[-1], self.d2), initializer='glorot_uniform',
-            trainable=True, dtype='float32'
-        )
-        self.b2 = self.add_weight(
-            shape=(self.d2,), initializer='glorot_uniform',
-            trainable=True, dtype='float32'
-        )
-
-    def call(self, x):
-        ff1 = tf.nn.relu(tf.matmul(x, self.W1) + self.b1)
-        ff2 = tf.matmul(x, self.W2) + self.b2
-        return ff2
-
-
-class EncoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d, n_heads):
-        super(EncoderLayer, self).__init__()
-        self.d = d
-        self.d_head = int(d / n_heads)
-        self.n_heads = n_heads
-
-    def build(self, input_shape):
-        self.attn_heads = [SelfAttentionLayer(self.d_head) for i in range(self.n_heads)]
-        self.fc_layer = FCLayer(2048, self.d)
-
-    def compute_multihead_output(self, x):
-        outputs = [head(x, x, x)[0] for head in self.attn_heads]
-        outputs = tf.concat(outputs, axis=-1)
-        return outputs
-
-    def call(self, x):
-        h1 = self.compute_multihead_output(x)
-        y = self.fc_layer(h1)
-        return h1
-
-
-class DecoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d, n_heads):
-        super(DecoderLayer, self).__init__()
-        self.d = d
-        self.d_head = int(d / n_heads)
-        self.dec_attn_heads = [SelfAttentionLayer(self.d_head) for i in range(n_heads)]
-        self.attn_heads = [SelfAttentionLayer(self.d_head) for i in range(n_heads)]
-        self.fc_layer = FCLayer(2048, self.d)
-
-    def call(self, de_x, en_x, mask=None):
-        def compute_multihead_output(de_x, en_x, mask=None):
-            outputs = [head(en_x, en_x, de_x, mask)[0] for head in self.attn_heads]
-            outputs = tf.concat(outputs, axis=-1)
-            return outputs
-
-        h1 = compute_multihead_output(de_x, de_x, mask)
-        h2 = compute_multihead_output(h1, en_x)
-        y = self.fc_layer(h2)
-        return y
-
-
 class ProjectToInitialCCS(tf.keras.layers.Layer):
     """
     Simple linear regression model, calculates ccs value as linear mapping from mz, charge -> ccs
@@ -176,10 +69,11 @@ class DeepAttentionModel(tf.keras.models.Model):
         self.__seq_len = seq_len
 
         self.linear = ProjectToInitialCCS(slopes, intercepts)
-        self.emb = tf.keras.layers.Embedding(input_dim=num_tokens + 1, output_dim=128, input_length=seq_len)
+        self.emb_1 = tf.keras.layers.Embedding(input_dim=num_tokens + 1, output_dim=128, input_length=seq_len)
+        self.emb_2 = tf.keras.layers.Embedding(input_dim=num_tokens + 1, output_dim=128, input_length=seq_len)
 
-        self.attn_1 = EncoderLayer(attn_dim, n_heads)
-        self.attn_2 = EncoderLayer(attn_dim, n_heads)
+        self.cnn_layer = tf.keras.layers.Conv1D(filters=100, kernel_size=8, padding='same')
+        self.attn = tf.keras.layers.Attention()
         self.gru = tf.keras.layers.GRU(gru_dim, return_sequences=False)
 
         self.dense1 = tf.keras.layers.Dense(128, activation='relu',
@@ -197,7 +91,16 @@ class DeepAttentionModel(tf.keras.models.Model):
         # get inputs
         mz, charge, seq, helix, gravy = inputs[0], inputs[1], inputs[2], inputs[3], inputs[4]
         # sequence learning
-        x_recurrent = self.gru(self.attn_2(self.attn_1(self.emb(seq))))
+
+        query_embeddings = self.emb_1(seq)
+        value_embeddings = self.emb_2(seq)
+
+        query_seq_encoding = self.cnn_layer(query_embeddings)
+        value_seq_encoding = self.cnn_layer(value_embeddings)
+
+        query_value_attention_seq = self.attn([query_seq_encoding, value_seq_encoding])
+        x_recurrent = self.gru(query_value_attention_seq)
+
         # concat to feed to dense layers
         concat = tf.keras.layers.Concatenate()([charge, x_recurrent, helix, gravy])
         # regularize
