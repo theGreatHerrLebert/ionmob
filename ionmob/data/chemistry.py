6daffa455bc006916dc2cc6cf3bb3829b67083af
theGreatHerrLebert <davidteschner@googlemail.com>
2022-05-23 10:30:30 +0200

    adding corrected datasets

--- ionmob/data/chemistry.py
+++ ionmob/data/chemistry.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pyopenms import *
 
 amino_acids = { 'Lysine': 'K',
@@ -69,3 +70,18 @@ def calculate_mz(sequence, charge):
         vanilla_mass += value * MODIFICATIONS_MZ[key]
 
     return seq, c_dict, (vanilla_mass + charge * MASS_PROTON) / charge
+
+
+def reduced_mobility_to_ccs(one_over_k0, mz, charge, mass_gas=28.013, temp=31.85, t_diff=273.15):
+    """
+    convert reduced ion mobility (1/k0) to CCS
+    :param oneOverK0: reduced ion mobility
+    :param charge: charge state of the ion
+    :param mz: mass over charge of the ion
+    :param mass_gas: mass of drift gas
+    :param temp: temperature of the drift gas in C°
+    :param t_diff: factor to translate from C° to K
+    """
+    MAGIC_NUMBER = 18509.8632163405
+    reduced_mass = (mz * charge * mass_gas) / (mz * charge + mass_gas)
+    return (MAGIC_NUMBER * charge) / (np.sqrt(reduced_mass * (temp + t_diff)) * 1/one_over_k0)
