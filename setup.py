7f1b8a266a192ec42c736b43fe3ac2c9af017c46
theGreatHerrLebert <davidteschner@googlemail.com>
2023-08-09 10:05:33 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent"
     ],
     long_description=ld,
