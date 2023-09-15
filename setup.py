7bf65066d8801c770953a2682c7549ba65273c40
RalfG <ralfg@hotmail.be>
2023-09-15 10:25:48 +0200

    Update PyPI classifiers

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ setup(
     #     "pretrained_models": ["*"],
     # },
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
